286af9f4aea6f41b08f6dc07874c8cfd98224876
Filip Troníček <filip.tronicek@seznam.cz>
2020-05-31 15:26:57 +0200

    Initial commit

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const port = process.env.PORT || 5000;
 
 const express = require("express");
 const fetch = require("node-fetch");
+const Base64 = require("js-base64").Base64;
 
 const app = express();
+
+const token = process.env.API_key || require("./creds").token;
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
@@ -15,21 +18,29 @@ app.get("/", function (req, res) {
   res.render("index");
 });
 
+app.post("/api", (req, res) => {
+  if (req.body.name) {
+    const vidName = "Test služby";
+    const body = {
+      event_type: "trigger",
+      client_payload: { name: vidName, id: Base64.encode(vidName) },
+    };
+
+    fetch("ttps://api.github.com/repos/filiptronicek/STC-Intro/dispatches", {
+      method: "post",
+      body: JSON.stringify(body),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "token "+token,
+      },
+    })
+      .then((result) => result.json())
+      .then((json) => res.json(json))
+      .catch(() => console.log("Success"))
+      .then(res.send("Success"));
+  }
+});
+
 app.listen(port, function () {
   console.log(`Editor listening on port ${port}!`);
-});
-app.get("/api", (req, res) => {
-  const body = {
-    event_type: "trigger",
-    client_payload: {name: "Test služby"},
-  };
-
-  fetch("https://api.github.com/repos/filiptronicek/STC-Intro/dispatches", {
-    method: "post",
-    body: JSON.stringify(body),
-    headers: { "Content-Type": "application/json", "Authorization": "token a1a6e52f80a7a9822b3046b770040024ddaa669a"},
-  })
-    .then((result) => result.json())
-    .then((json) => res.json(json))
-    .catch((err) => res.status(500).send);
 });
