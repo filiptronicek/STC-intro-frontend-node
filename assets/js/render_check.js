0f14e4185b75a14364c9b8b9d3686ab7b54398fe
Filip Troníček <filip.tronicek@seznam.cz>
2020-07-02 00:27:34 +0200

    Formatting

--- assets/js/render_check.js
+++ assets/js/render_check.js
@@ -7,24 +7,22 @@ const filelink = `https://github.com/filiptronicek/STC-intro/raw/master/render/$
 const time = 131;
 let timeleft = time;
 
-function UrlExists(url)
-{
-    const http = new XMLHttpRequest();
-    http.open('HEAD', url, false);
-    http.send();
-    //return http.status!=404;
-    return true;
+function UrlExists(url) {
+  const http = new XMLHttpRequest();
+  http.open("HEAD", url, false);
+  http.send();
+  //return http.status!=404;
+  return true;
 }
 
 function checkForFile() {
-    if(!UrlExists("https://cors-anywhere.herokuapp.com/"+filelink)) {
-      timeleft = 10;
-
-    } else {
-      clearInterval(timeInterval);
-      linkDiv.innerHTML = `Link: <a href="${filelink}" download>Download</a>`;
-      countdownDiv.style.display = "none";
-    }
+  if (!UrlExists("https://cors-anywhere.herokuapp.com/" + filelink)) {
+    timeleft = 10;
+  } else {
+    clearInterval(timeInterval);
+    linkDiv.innerHTML = `Link: <a href="${filelink}" download>Download</a>`;
+    countdownDiv.style.display = "none";
+  }
 }
 
 function countDown() {
